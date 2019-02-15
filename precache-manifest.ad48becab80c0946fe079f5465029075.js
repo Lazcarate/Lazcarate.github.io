464102baf199c3640221e1cbb63485ee40f5c884
Luis <lazcarate@movistar.es>
2019-02-15 11:37:00 +0100

    primer commit

---
+++ precache-manifest.ad48becab80c0946fe079f5465029075.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a9d34cbe65efe75e7a5",
+    "url": "/static/css/main.42fa6f6d.chunk.css"
+  },
+  {
+    "revision": "7a9d34cbe65efe75e7a5",
+    "url": "/static/js/main.7a9d34cb.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0aeb68bc986453b09c684e35ac54b542",
+    "url": "/index.html"
+  }
+];
